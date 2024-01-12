89d4ef600f0761eade52887a089a77b040e3042d
Yaswanth Bangaru <yaswanthbangaru13@gmail.com>
2024-01-12 13:38:29 +0100

    parallel processing

--- pipeline_build_nx_graphs.py
+++ pipeline_build_nx_graphs.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 import pickle
 from helpers import logger, NO_JOBS
 from joblib import Parallel, delayed
+import multiprocessing
 # import torch.nn as nn
 
 from helpers import spatialPipeline, ANNOTATION_DICT, plotly_spatial_scatter_edges, plotly_spatial_scatter_categorical
@@ -19,102 +20,104 @@ from helpers import build_graph_from_cell_coords, assign_attributes, calcualte_v
 from helpers import BoundaryDataLoader, SLICE_PIXELS_EDGE_CUTOFF, logger
 import matplotlib.pyplot as plt
 
+all_liver_samples = ["Liver1Slice1", "Liver1Slice2", "Liver2Slice1", "Liver2Slice2"]
+data_filter_name = "Liver12Slice12"
+leiden_resolution = 0.7
+
+data_options = {
+    "dir_loc" : f"/data/qd452774/spatial_transcriptomics/",
+    "names_list" : all_liver_samples,
+    "mlflow_config" : {
+        "id"  : None,
+        "data_filter_name" : data_filter_name,
+        "leiden_resolution" : leiden_resolution,
+    },
+}
+pipeline_instance = spatialPipeline(options=data_options)
+pipeline_instance.data.obs['cell_type'] = pipeline_instance.data.obs['leiden_res'].map(ANNOTATION_DICT)
+
+
+NODE_FEATURES = ["cell_type", "center_coord", "biomarker_expression", "volume", "boundary_polygon", 'cell_id', 'is_padding', 'perimeter', 'area', 'mean_radius', 'compactness_circularity', 'fractality', 'concavity', 'elongation', 'overlap_index', 'sbro_orientation', 'wswo_orientation']
+EDGE_FEATURES = ["distance", "edge_type"]
+
+
+pretransform_networkx_configs = [
+    {
+        "boundary_type" : "voronoi",
+        "edge_config" : {
+            "type" : "Delaunay",
+            "bound_type" : "",
+        },
+        "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
+    },
+    {
+        "boundary_type" : "given",
+        "edge_config" : {
+            "type" : "Delaunay",
+            "bound_type" : "",
+        },
+        "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
+    },        
+    {
+        "boundary_type" : "given",
+        "edge_config" : {
+            "type" : "R3Index",
+            "bound_type" : "rectangle",
+            "threshold_distance" : 0,
+        },    
+        "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
+    }, 
+    {
+        "boundary_type" : "given",
+        "edge_config" : {
+            "type" : "R3Index",
+            "bound_type" : "rotated_rectangle",
+            "threshold_distance" : 0,
+        },    
+        "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
+    },                
+
+]
+
+all_slices_names_grid = []
+for item_slice in all_liver_samples:
+    for x_index in list(range(20)):
+        for y_index in list(range(20)):
+            temp_segment_config = {
+                "segments_per_dimension" : 20,
+                "sample_name" : item_slice,
+                "region_id" : (x_index, y_index),
+                "padding" : {
+                    "value" : SLICE_PIXELS_EDGE_CUTOFF[item_slice]*40,
+                    "units" : "pixels",
+                },
+            }                
+            all_slices_names_grid.append(temp_segment_config)
+
+network_features_config = {
+    "node_features" : NODE_FEATURES,
+    "edge_features" : EDGE_FEATURES,
+}
+
+def parallel_function(segment_config, pretransform_networkx_config, network_features_config):
+    try:
+        curr_nx_graph = pipeline_instance.build_networkx_for_region(segment_config, pretransform_networkx_config, network_features_config)
+        pipeline_instance.build_networkx_plots(curr_nx_graph, segment_config, pretransform_networkx_config, save_=True)
+    except Exception as e:
+        logger.info("------------------")
+        logger.info(segment_config.values())
+        logger.error(f"{str(e)}")
 
 def main():
-    # y_data_filter_name = "Liver12Slice12"
-    # y_resolution = 0.7
-    # ann_data = read_run_result_ann_data(y_data_filter_name, y_resolution)
 
-    # ann_data.obs = ann_data.obs.rename(columns={"sample": "liver_slice"})
-    # ann_data.obs['CELL_TYPE'] = ann_data.obs['leiden_res'].tolist() 
+    func_args = [(segment_config, pretransform_networkx_config, network_features_config)
+                 for segment_config in all_slices_names_grid
+                 for pretransform_networkx_config in pretransform_networkx_configs]            
 
-    all_liver_samples = ["Liver1Slice1", "Liver1Slice2", "Liver2Slice1", "Liver2Slice2"]
-    data_filter_name = "Liver12Slice12"
-    leiden_resolution = 0.7
-
-    data_options = {
-        "dir_loc" : f"/data/qd452774/spatial_transcriptomics/",
-        "names_list" : all_liver_samples,
-        "mlflow_config" : {
-            "id"  : None,
-            "data_filter_name" : data_filter_name,
-            "leiden_resolution" : leiden_resolution,
-        },
-    }
-    pipeline_instance = spatialPipeline(options=data_options)
-    pipeline_instance.data.obs['cell_type'] = pipeline_instance.data.obs['leiden_res'].map(ANNOTATION_DICT)
-
-
-    NODE_FEATURES = ["cell_type", "center_coord", "biomarker_expression", "volume", "boundary_polygon"]
-    EDGE_FEATURES = ["distance", "edge_type"]
-
-    segment_config = {
-        "segments_per_dimension" : 20,
-        "sample_name" : "Liver1Slice1",
-        "region_id" : (14, 17),
-        "padding" : {
-            "value" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"]*40,
-            "units" : "pixels",
-        },
-    }
-
-    pretransform_networkx_configs = [
-        {
-            "boundary_type" : "voronoi",
-            "edge_config" : {
-                "type" : "Delaunay",
-            },
-            "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
-        },
-        {
-            "boundary_type" : "given",
-            "edge_config" : {
-                "type" : "Delaunay",
-            },
-            "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
-        },        
-        {
-            "boundary_type" : "given",
-            "edge_config" : {
-                "type" : "R3Index",
-                "bound_type" : "rectangle",
-                "threshold_distance" : 0,
-            },    
-            "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
-        }, 
-        {
-            "boundary_type" : "given",
-            "edge_config" : {
-                "type" : "R3Index",
-                "bound_type" : "rotated_rectangle",
-                "threshold_distance" : 0,
-            },    
-            "neighbor_edge_cutoff" : SLICE_PIXELS_EDGE_CUTOFF["Liver1Slice1"],
-        },                
-
-    ]
-
-    network_features_config = {
-        "node_features" : NODE_FEATURES,
-        "edge_features" : EDGE_FEATURES,
-    }
-
-    for pretransform_networkx_config in pretransform_networkx_configs:
-        curr_nx_graph = pipeline_instance.build_networkx_for_region(segment_config, pretransform_networkx_config, network_features_config)
-        pipeline_instance.build_networkx_plots(curr_nx_graph, pretransform_networkx_config, save_=True)
-
-
-    dataset_root = f"/data/qd452774/spatial_transcriptomics/data/{y_data_filter_name}_leiden_res_{y_resolution}"
-    nx_graph_root = os.path.join(dataset_root, f"graph_{y_data_filter_name}_{y_resolution}")
-    all_graph_names = sorted([f for f in os.listdir(nx_graph_root) if f.endswith('_voronoi.gpkl')])
-    nx_graph_files = [os.path.join(nx_graph_root, f) for f in all_graph_names]
-
-    from helpers import get_biomarker_metadata
-    biomarkers_list, _ = get_biomarker_metadata(nx_graph_files)
-    save_biomarkers_list = os.path.join(dataset_root, "biomarkers_list.csv")
-    biomarkers_df = pd.DataFrame(biomarkers_list)
-    biomarkers_df.to_csv(save_biomarkers_list, index=False, header=False)
-    logger.info(f"Saved biomarkers list to {save_biomarkers_list}")
+    # Create a multiprocessing Pool and execute the function in parallel
+    with multiprocessing.Pool(processes=NO_JOBS) as pool:
+        pool.starmap(parallel_function, func_args)
+        
 
 if __name__=="__main__":
     main()
